e0ccb298a229dbf1a788d28f03cc529988b979aa
cameron <cmashton2013@gmail.com>
2022-08-10 14:56:43 -0600

    first 3 done

---
+++ 4day/lecture.js
@@ -0,0 +1,18 @@
+// let hungry = false
+
+// function sayHungry(){
+
+//     hungry = true
+
+//     if (hungry){
+//         console.log("I'm hungry!")
+//     } else {
+//         console.log("I'm coding!")
+//     }
+
+// }
+// sayHungry()
+
+// console.log("Value of hungry", hungry)
+
+
